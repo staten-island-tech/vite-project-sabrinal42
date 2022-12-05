0a1ea6fc3e0363a0ddf2c0ccf18ee1343e34bdb8
sabrinal42 <78661937+sabrinal42@users.noreply.github.com>
2022-12-05 13:59:58 -0500

    YAS

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector(".btn").addEventListener("click", function () {
+  if (document.body.classList.contains("light")) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
+  }
+});
